c71b72d5ecca4d2019e5ca0de8669314a1f90be7
David <caldwell987@gmail.com>
2025-06-10 15:53:45 -0500

    Save all local changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  // basePath: "/MepScans-Website",
-  // assetPrefix: "/MepScans-Website/",
-  // trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
